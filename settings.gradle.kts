90362d8bc228e071727abeca54782d41bc0ea56e
emykr <bravegirl0904@gmail.com>
2025-08-09 20:32:01 +0900

    refactor build configuration to consolidate repository definitions in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/") {
+            name = "papermc-repo"
+        }
+        maven("https://jitpack.io")
+    }
 }
 rootProject.name = "SommandAPI"
